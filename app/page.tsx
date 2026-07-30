1996d23e9ffe612ce8ce15c713e2c332f3e5b3dc
Guizaoo <guilhermexxzz54@gmail.com>
2026-07-30 13:12:17 -0300

    style: tirnado comentario

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,6 @@
 import ReservationDashboard from "@/app/ui/reservation-dashboard";
 
-// A página continua sendo um Server Component.
-// A parte interativa fica isolada no ReservationDashboard.
+
 export default function Home() {
   return <ReservationDashboard />;
 }
